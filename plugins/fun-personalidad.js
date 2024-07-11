02eba66469ad4b0a0b240f80013cb8708b91ccdc
Aldair-ld <frankaldair29@gmail.com>
2024-07-11 15:59:51 -0500

    Update fun-personalidad.js

--- plugins/fun-personalidad.js
+++ plugins/fun-personalidad.js
@@ -1,13 +1,14 @@
 let handler = async (m, { conn }) => {
   let fkontak = { 
     "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, 
-    "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Soporte;Bot;;;\nFN:Soporte Bot\nitem1.TEL;waid=51912345678:+51 912 345 678\nitem1.X-ABLabel:Soporte\nEND:VCARD` }},
+    "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Soporte;Bot;;;\nFN:Soporte Bot\nitem1.TEL;waid=51925015528:+51 925 015 528\nitem1.X-ABLabel:Soporte\nEND:VCARD` }},
     "participant": "0@s.whatsapp.net" 
   };
 
   let contactInfo = `*📞 Información de Contacto del Soporte*\n\n` +
-                    `*Nombre:* ALDAIR\n` +
-                    `*Número:* +51 925 015 528\n` 
+                    `*Nombre:* Soporte Bot\n` +
+                    `*Número:* +51 925 015 528\n` +
+                    `*Horario de Atención:* 10:00 AM - 11:59 PM (Hora Local)\n\n` +
                     `Para cualquier consulta o soporte técnico, por favor contacta al número proporcionado.`;
 
   await conn.reply(m.chat, contactInfo, fkontak, m);
